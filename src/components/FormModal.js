c043d6c13c34fcf7b21531d0370476f1de0687ca
Shani Chulin <shaninchulin@gmail.com>
2020-06-06 13:15:51 -0400

    update

--- src/components/FormModal.js
+++ src/components/FormModal.js
@@ -7,17 +7,14 @@ const FormModal = () => {
     { value: "medium", displayName: "Medium" },
     { value: "large", displayName: "Large" },
   ];
+
   const milkRadioButtons = [
     { value: "no milk", displayName: "No Milk" },
     { value: "whole milk", displayName: "Whole Milk" },
     { value: "soy milk", displayName: "Soy Milk" },
     { value: "almond milk", displayName: "Almond Milk" },
   ];
-  const sweetRadioButtons = [
-    { value: "no-sugar", displayName: "No-Sugar" },
-    { value: "one spoon", displayName: "One Spoon" },
-    { value: " double-double", displayName: " Double-Double" },
-  ];
+
   const {
     sizeHandleChange,
     milkHandleChange,
@@ -33,87 +30,59 @@ const FormModal = () => {
   return (
     <React.Fragment>
       <form action="#" onSubmit={handleSubmit}>
-        <fieldset className="form-group size">
-          <div className="row">
-            <div className="col-sm-2 pt-0">
-              <h5 className="title">size</h5>
-              {sizeRadioButtons.map(({ value, displayName }, idx) => (
-                <div className="form-check">
+        <div>
+          <fieldset className="form-group size">
+            <div className="row">
+              <div className="col-sm-2 pt-0">
+                <h5 className="title">size</h5>
+                {sizeRadioButtons.map(({ value, displayName }, idx) => (
                   <div key={`size-${idx}`}>
-                    <label className="form-check-label">
-                      <input
-                        className="form-check-input"
-                        type="radio"
-                        id={idx}
-                        name="size"
-                        value={value}
-                        checked={size === value}
-                        onChange={sizeHandleChange}
-                      />
-
-                      {displayName}
-                    </label>
+                    <div className="form-check">
+                      <label className="form-check-label">
+                        <input
+                          className="form-check-input"
+                          type="radio"
+                          id={`size-${idx}`}
+                          name="size"
+                          value={value}
+                          checked={size === value}
+                          onChange={sizeHandleChange}
+                        />
+                        {console.log(id)}
+                        {displayName}
+                      </label>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
-          </div>
-        </fieldset>
-
-        <fieldset className="form-group milk">
-          <div className="row">
-            <div className="col-sm-10">
-              <h5 className="title pt-4">milk</h5>
-              {milkRadioButtons.map(({ value, displayName }, idy) => (
-                <div key={`size-${idy}`}>
+          </fieldset>
+        </div>
+        <div>
+          <fieldset className="form-group milk">
+            <div className="row">
+              <div className="col-sm-10">
+                <h5 className="title pt-4">milk</h5>
+                {milkRadioButtons.map(({ value, displayName }, idx) => (
                   <div className="form-check">
                     <label className="form-check-label">
                       <input
                         className="form-check-input"
                         type="radio"
-                        id={idy}
+                        id={`milk-${idx}`}
                         name="milk"
                         value={value}
                         checked={milk === value}
                         onChange={milkHandleChange}
                       />
-
                       {displayName}
                     </label>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
-          </div>
-        </fieldset>
-
-        <fieldset className="form-group sugar">
-          <div className="row">
-            <div className="col-sm-10">
-              <h5 className="title pt-4">Sweetner</h5>
-              {sweetRadioButtons.map(({ value, displayName }, idz) => (
-                <div className="form-check">
-                  <div key={`sweet_${idz}`}>
-                    <label className="form-check-label">
-                      <input
-                        className="form-check-input"
-                        type="radio"
-                        id={idz}
-                        name="sweet"
-                        value={value}
-                        checked={sweet === value}
-                        //onChange helps me to put what the client chose in the state
-                        onChange={sweetHandleChange}
-                      />
-
-                      {displayName}
-                    </label>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        </fieldset>
+          </fieldset>
+        </div>
 
         <div className="d-flex justify-content-center py-3">
           <h4 className="px-2">Price {price} $</h4>
--- src/components/Product.js
+++ src/components/Product.js
@@ -8,18 +8,19 @@ const Product = ({ product }) => {
 
   return (
     <div className="col-8 mx-auto col-lg-4 col-md-4">
-      <div className="card mt-3"></div>
-      <div className="img-container p-3">
-        <div className="coffeType">
-          <img
-            src={img}
-            alt="coffe type"
-            className="card-img-top"
-            onClick={() => openModal(id)}
-          />
-          <div className="d-flex justify-content-between pb-3">
-            <h5 className="title">{title}</h5>
-            <h5 className="price">{price}$</h5>
+      <div className="card mt-3">
+        <div className="img-container p-3">
+          <div className="coffeType">
+            <img
+              src={img}
+              alt="coffe type"
+              className="card-img-top"
+              onClick={() => openModal(id)}
+            />
+            <div className="d-flex justify-content-between pb-3">
+              <h5 className="title">{title}</h5>
+              <h5 className="price">{price}$</h5>
+            </div>
           </div>
         </div>
       </div>
